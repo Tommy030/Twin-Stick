d5ac4372ec4325291a1b2b52d11fc707d9e47c2c
Tommy Sett <533656@student.glu.nl>
2020-05-16 02:09:24 +0200

    kjhk

---
+++ Assets/Scripts/Enemy/EnemySight.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySight : MonoBehaviour
+{
+  [SerializeField]  public float FOVA = 110f;
+    public bool PlayerSEE;
+    public float LastLocPlayerSighted;
+}
---
+++ Assets/Scripts/Enemy/Scriptable Enemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Enemy", menuName = "Enemy")]
+public class ScriptableEnemy : ScriptableObject
+{
+
+    
+    public float HP;
+    public float MovementSpeed; 
+    
+    public float Damage;
+    public float KniveDamage;
+
+    public float FireRate;
+
+    //De Enemy Reload. 
+    public float AmmoMax;
+    public float AmmoInClip; 
+}
