acb804ab8c481edeaa61eda8d400388cce7474bf
Robin van den Dungen <533124@student.glu.nl>
2020-05-29 14:13:53 +0200

    Door
    YEEEET

--- Assets/Scripts/DoorOrOthershizzle/Door.cs
+++ Assets/Scripts/DoorOrOthershizzle/Door.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-
+    private bool isOpening = false;
+    private bool isClosing = false;
+    private bool isOpen = false;
+    private float DoorTime = 0f;
+    [SerializeField] private bool isVertical;
+    [SerializeField] private BoxCollider Box;
+    [SerializeField] private float OpenSpeed = 1f;
     [Tooltip("Als de deur een Keyard nodig heeft klik op dit vakje en gooi die key erin. ")]
     [SerializeField] private bool KeyCard ; 
     [SerializeField]KeyCardScriptable ThisDoorsKeyID;
@@ -26,7 +32,11 @@ public class Door : MonoBehaviour
                     {
                         if (Card == ThisDoorsKeyID)
                         {
-                            Opendeur();
+                            if (!isOpen)
+                            {
+                              Opendeur();
+
+                            }
                             //een code om whatever deur nu te doen weetj. idk... geen zin in. als t goed is werkt dit idk. we zien wel
                         }
 
@@ -35,13 +45,73 @@ public class Door : MonoBehaviour
                 }
             if (!KeyCard)
                 {
-                    Opendeur();
+                    if (!isOpen)
+                    {
+                        Opendeur();
+
+                    }
                 }
             }
         }
     }
     void Opendeur()
     {
-        //de code
+        isOpening = true;
     }
+    private void Update()
+    {
+        if (isOpening)
+        {
+            Box.enabled = false;
+            DoorTime += Time.deltaTime;
+            if (DoorTime >= 3f)
+            {
+                isOpening = false;
+                DoorTime = 0f;
+                isOpen = true;
+            }
+            if (isVertical)
+            {
+                transform.position += (Vector3.forward * OpenSpeed) * Time.deltaTime;
+            }
+            else
+            {
+               transform.position += (Vector3.right * OpenSpeed) * Time.deltaTime;
+            }
+      
+        }
+        else
+        {
+            Box.enabled = true;
+        }
+        if (isOpen)
+        {
+            DoorTime += Time.deltaTime;
+            if (DoorTime >= 6f)
+            {
+                DoorTime = 0f;
+                isClosing = true;
+                isOpen = false;
+            }
+        }
+        if (isClosing)
+        {
+            Box.enabled = false;
+            DoorTime += Time.deltaTime;
+            if (DoorTime >= 3f)
+            {
+                isClosing = false;
+                DoorTime = 0f;
+            }
+            if (isVertical)
+            {
+                transform.position -= (Vector3.forward * OpenSpeed) * Time.deltaTime;
+            }
+            else
+            {
+                transform.position -= (Vector3.right * OpenSpeed) * Time.deltaTime;
+            }
+        }
+    }
+    
 }
---
+++ Assets/Scripts/UI/StartScreenManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreenManager : MonoBehaviour
+{
+    [SerializeField] private float KeyCooldown;
+    private float KeyCooldownTimer;
+    private void Update()
+    {
+        if (KeyCooldownTimer >= KeyCooldown)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+            else if (Input.anyKey)
+            {
+                SceneManager.LoadScene("SelectMenu");
+            }
+
+        }
+        else
+        {
+            KeyCooldownTimer += Time.deltaTime;
+        }
+
+    }
+}
